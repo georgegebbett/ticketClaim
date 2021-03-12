55e7498df425b1a1b1fa983bf4d6c437c7cb6636
George Gebbett <george.gebbett1@gmail.com>
2021-03-12 14:15:05 +0000

    options page works yes

--- zdClaimContentScript.js
+++ zdClaimContentScript.js
@@ -1,18 +1,20 @@
 console.log("Zendesk ticket claim content script loaded");
 
+buttonList = [];
+
 var observer = new MutationObserver(function (mutations, observer) {
     for (var mut = 0; mut < mutations.length; mut++) {
         try {
             // console.log("Tab change");
-            var claimButton = document.createElement("span");
-            claimButton.className = "ember-view btn";
-            claimButton.classList.add("claimBtn");
-            claimButton.innerText = "Claim";
-            claimButton.addEventListener('click', claimTicket);
             var allBtnGroups = document.getElementsByClassName("ember-view btn-group");
             for (let item of allBtnGroups) {
                 if (!item.classList.contains("claimBtnAdded") && !item.classList.contains("apps_group")) {
                     // console.log("append");
+                    let claimButton = document.createElement("span");
+                    claimButton.className = "ember-view btn";
+                    claimButton.classList.add("claimBtn");
+                    claimButton.innerText = "Claim";
+                    claimButton.addEventListener('click', claimTicket);
                     item.appendChild(claimButton);
                     item.classList.add("claimBtnAdded");
                 }
